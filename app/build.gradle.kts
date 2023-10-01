33514b73df28139a3ccb2d65bc0fbca7c63471fe
Nuttapong Pantikam <nuttapong.pan@truedigital.com>
2023-10-01 23:49:31 +0700

    add authen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
 
 kapt {
